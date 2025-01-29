bbca48a355206ed18dd8b4161cf9b42dc02bb92a
dkh0387 <deniskh87@gmail.com>
2025-01-29 17:11:14 +0100

    feat: loading indicators and custom not found page

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,9 @@
+import {Loader2} from "lucide-react";
+
+/**
+ * Loading indicator for the main page.
+ * @constructor
+ */
+export default function Loading() {
+  return <Loader2 className="mx-auto my-3 animate-spin" />;
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import {Loader2} from "lucide-react";
+
+/**
+ * Loading indicator for all root pages.
+ * @constructor
+ */
+export default function Loading() {
+  return <Loader2 className="mx-auto my-3 animate-spin" />;
+}

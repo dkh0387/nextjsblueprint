e7a906e8b36975500559a38366069e7e45cd0f41
dkh0387 <deniskh87@gmail.com>
2025-02-14 20:03:41 +0100

    feat: selecting multiple users for a group chat

--- src/app/(main)/messages/NewChatDialog.tsx
+++ src/app/(main)/messages/NewChatDialog.tsx
@@ -1,12 +1,13 @@
 import {Dialog, DialogContent, DialogHeader, DialogTitle,} from "@/components/ui/dialog";
-import {DefaultStreamChatGenerics, useChatContext,} from "stream-chat-react";
+import {DefaultStreamChatGenerics, useChatContext} from "stream-chat-react";
 import {useToast} from "@/components/ui/use-toast";
 import {useState} from "react";
 import {useSession} from "@/app/(main)/SessionProvider";
 import useDebounce from "@/hooks/useDebounce";
 import {UserResponse} from "stream-chat";
 import {useQuery} from "@tanstack/react-query";
-import {Search} from "lucide-react";
+import {Check, Search, X} from "lucide-react";
+import UserAvatar from "@/components/UserAvatar";
 
 /**
  * New Chat dialog, opens by click on MailPlus icon in header of chat sidebar.
@@ -72,8 +73,89 @@ export default function NewChatDialog(props: NewChatDialogProps) {
               onChange={(e) => setSearchInput(e.target.value)}
             />
           </div>
+          {selectedUSers.length > 0 && (
+            <div className="mt-4 flex flex-wrap gap-2 p-2">
+              {selectedUSers.map((user) => (
+                <SelectedUserTag
+                  user={user}
+                  key={user.id}
+                  onRemove={() => {
+                    setSelectedUSers((prev) =>
+                      prev.filter((u) => u.id !== user.id),
+                    );
+                  }}
+                ></SelectedUserTag>
+              ))}
+            </div>
+          )}
+          <hr />
+          <div className="h-96 overflow-y-auto">
+            {isSuccess &&
+              data?.users.map((user) => (
+                <UserResult
+                  key={user.id}
+                  user={user}
+                  selected={selectedUSers.some((u) => u.id === user.id)}
+                  onClick={() => {
+                    setSelectedUSers((prev) =>
+                      prev.some((u) => u.id === user.id)
+                        ? prev.filter((u) => u.id !== user.id)
+                        : [...prev, user],
+                    );
+                  }}
+                />
+              ))}
+          </div>
         </div>
       </DialogContent>
     </Dialog>
+  );
+}
+
+/**
+ * Search result as a user avatar with name.
+ */
+interface UserResultProps {
+  user: UserResponse<DefaultStreamChatGenerics>;
+  selected: boolean;
+  onClick: () => void;
+}
+
+function UserResult(props: UserResultProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      className="hover: flex w-full items-center justify-between bg-muted/50 px-4 py-2.5 transition-colors"
+    >
+      <div className="flex items-center gap-2">
+        <UserAvatar avatarUrl={props.user.image} />
+        <div className="flex flex-col text-start">
+          <p className="font-bold">{props.user.name}</p>
+          <p className="text-muted-foreground">@{props.user.username}</p>
+        </div>
+      </div>
+      {props.selected && <Check className="size-5 text-green-500" />}
+    </button>
+  );
+}
+
+/**
+ * Tag for a selected user for (group)chat.
+ */
+interface SelectedUserTagProps {
+  user: UserResponse<DefaultStreamChatGenerics>;
+  onRemove: () => void;
+}
+
+function SelectedUserTag(props: SelectedUserTagProps) {
+  return (
+    <button
+      onClick={props.onRemove}
+      className="flex items-center gap-2 rounded-full border p-1 hover:bg-muted/50"
+    >
+      <UserAvatar avatarUrl={props.user.image} size={24} />
+      <p className="font-bold">{props.user.name}</p>
+      <X className="mx-2 size-5 text-muted-foreground" />
+    </button>
   );
 }
